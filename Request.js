585cc289b423a9f80378cbbda1af76df814fdb5f
vrctake <76068812+vrctake@users.noreply.github.com>
2020-12-15 13:25:39 -0600

    Update Request.js

--- Request.js
+++ Request.js
@@ -1,1 +1,22 @@
+import npm
+      
+var requestPromise = require('request-promise');
 
+var user = 'irtsa';
+var password = 'dufuxgkfywohkf';
+
+var base64encodedData = Buffer.from(user + ':' + password).toString('base64');
+
+alert(requestPromise.get({
+  uri: 'https://api.vrchat.cloud/api/1/auth/user',
+  headers: {
+    'Authorization': 'Basic ' + base64encodedData
+  },
+  json: true
+})
+.then(function ok(jsonData) {
+  console.dir(jsonData);
+})
+.catch(function fail(error) {
+  // handle error
+}));
